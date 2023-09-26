7a7f19c6b14589e837ff1c9a0b0a5daff00c5651
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-09-26 17:36:24 +0600

    padding fixed of homepage

--- inventory-management-client/app/components/Home/HomePage.jsx
+++ inventory-management-client/app/components/Home/HomePage.jsx
@@ -4,8 +4,8 @@ import LineChartDemo from '../Charts/LineChart'
 
 const HomePage = () => {
     return (
-        <div className='overflow-hidden w-full min-h-[100vh] py-4 px-6 '>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mb-6'>
+        <div className='overflow-hidden w-full min-h-[100vh] p-2 lg:p-6 '>
+            <div className='grid gap-2 lg:gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mb-2 lg:mb-6'>
 
                 <div className="bg-white shadow p-4 rounded-md">
                     <div className='flex flex-col gap-6'>
@@ -85,7 +85,7 @@ const HomePage = () => {
                 </div>
 
             </div>
-            <div className='w-[100%] bg-white py-6 shadow-md rounded-lg pr-4 '>
+            <div className='w-[100%] bg-white p-2 lg:p-6 shadow-md rounded-lg pr-4 '>
                 <div className='flex gap-6 mb-6 ml-6'>
                     <div className='flex gap-2 items-center'>
                         <FaCircleDot className='text-[#5A66F1]' />
