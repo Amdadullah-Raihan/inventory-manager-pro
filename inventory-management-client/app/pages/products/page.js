64e6d6c2b4b4dbcbd5f79193e7d8aeffae1857a8
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-10-05 19:52:35 +0600

    changes the loading design in products and invoices pages

--- inventory-management-client/app/pages/products/page.js
+++ inventory-management-client/app/pages/products/page.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import { AiOutlineFolderView, AiOutlinePlus } from 'react-icons/ai';
 import { TbTrash } from 'react-icons/tb';
+import { RotatingLines } from 'react-loader-spinner';
 import { ToastContainer, toast } from 'react-toastify';
 
 const Products = () => {
@@ -76,99 +77,111 @@ const Products = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="overflow-x-auto ">
+                {
+                    isLoading ? <div className='flex gap-1 justify-center pb-8'>
+                        <p className='text-lg font-bold'>Loading</p>
+                        <RotatingLines
+                            strokeColor="#5A5FE0"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="24"
+                            visible={true}
+                        />
+                    </div> :
+                        <div className="overflow-x-auto ">
 
-                    <table className="table capitalize">
-                        {/* head */}
-                        <thead className='bg-base-200'>
-                            <tr>
-                                <th>
-                                    <label>
-                                        <input type="checkbox" className="checkbox" />
-                                    </label>
-                                </th>
-                                <th>Serial Number</th>
-                                <th>Product Name</th>
-                                <th>Stock</th>
-                                <th>Purchased From</th>
-                                <th>Actions</th>
+                            <table className="table capitalize">
+                                {/* head */}
+                                <thead className='bg-base-200'>
+                                    <tr>
+                                        <th>
+                                            <label>
+                                                <input type="checkbox" className="checkbox" />
+                                            </label>
+                                        </th>
+                                        <th>Serial Number</th>
+                                        <th>Product Name</th>
+                                        <th>Stock</th>
+                                        <th>Purchased From</th>
+                                        <th>Actions</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* row 1 */}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {/* row 1 */}
 
-                            {
-                                isLoading ? <p>Loading...</p> :
-
-
-                                    productsList?.length > 0 ? productsList?.map((product, idx) =>
-
-                                        <tr key={product._id}>
-                                            <td>
-                                                <label>
-                                                    <input type="checkbox" className="checkbox" />
-                                                </label>
-                                            </td>
-                                            <td className='text-[#5A5FE0] font-semibold'>
-                                                #{product.barCode}
-                                            </td>
+                                    {
 
 
-                                            <td>
-                                                {product.productName}
-                                            </td>
-                                            <td>{product.stock}</td>
-                                            <td>
-                                                <div className="flex items-center space-x-3">
-                                                    <div>
-                                                        <div className="font-bold">{product.purchasedFrom.shopName}</div>
-                                                        <div className="text-sm opacity-50">{product.purchasedFrom.shopAddress}</div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <button
-                                                    className="btn btn-ghost btn-xs"
-                                                    onClick={() => document.getElementById('my_modal_3').showModal()}
-                                                >
-                                                    <TbTrash className='text-2xl text-rose-500' />
-                                                </button>
-                                                <button className="btn btn-ghost btn-xs">
-                                                    <AiOutlineFolderView className='text-2xl text-[#5A5FE0]' />
-                                                </button>
-                                            </td>
-                                            {/* delete confirmation modal */}
-                                            <dialog id="my_modal_3" className="modal">
-                                                <div className="modal-box">
-                                                    <form method="dialog">
-                                                        {/* if there is a button in form, it will close the modal */}
-                                                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                                                    </form>
-                                                    <h3 className="font-bold text-l uppercase">Are you sure want to <span className='text-rose-500'>delete</span> the invoice?</h3>
-                                                    <div>
-                                                        <div className="modal-action">
-                                                            <form method="dialog">
 
-                                                                <button className="btn bg-green-500 text-white mr-2 hover:text-green-500" >Cancel</button>
-                                                                <button className='btn bg-rose-500 text-white hover:text-rose-500' onClick={() => handleDeleteProduct(product._id)}>Delete</button>
-                                                            </form>
+                                        productsList?.length > 0 ? productsList?.map((product, idx) =>
+
+                                            <tr key={product._id}>
+                                                <td>
+                                                    <label>
+                                                        <input type="checkbox" className="checkbox" />
+                                                    </label>
+                                                </td>
+                                                <td className='text-[#5A5FE0] font-semibold'>
+                                                    #{product.barCode}
+                                                </td>
+
+
+                                                <td>
+                                                    {product.productName}
+                                                </td>
+                                                <td>{product.stock}</td>
+                                                <td>
+                                                    <div className="flex items-center space-x-3">
+                                                        <div>
+                                                            <div className="font-bold">{product.purchasedFrom.shopName}</div>
+                                                            <div className="text-sm opacity-50">{product.purchasedFrom.shopAddress}</div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </dialog>
-                                        </tr>
+                                                </td>
+                                                <td>
+                                                    <button
+                                                        className="btn btn-ghost btn-xs"
+                                                        onClick={() => document.getElementById('my_modal_3').showModal()}
+                                                    >
+                                                        <TbTrash className='text-2xl text-rose-500' />
+                                                    </button>
+                                                    <button className="btn btn-ghost btn-xs">
+                                                        <AiOutlineFolderView className='text-2xl text-[#5A5FE0]' />
+                                                    </button>
+                                                </td>
+                                                {/* delete confirmation modal */}
+                                                <dialog id="my_modal_3" className="modal">
+                                                    <div className="modal-box">
+                                                        <form method="dialog">
+                                                            {/* if there is a button in form, it will close the modal */}
+                                                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                                        </form>
+                                                        <h3 className="font-bold text-l uppercase">Are you sure want to <span className='text-rose-500'>delete</span> the invoice?</h3>
+                                                        <div>
+                                                            <div className="modal-action">
+                                                                <form method="dialog">
 
-                                    ) :
-                                        <div className='my-16 text-center text-xl uppercase text-rose-500'> No products Found! </div>
-                            }
+                                                                    <button className="btn bg-green-500 text-white mr-2 hover:text-green-500" >Cancel</button>
+                                                                    <button className='btn bg-rose-500 text-white hover:text-rose-500' onClick={() => handleDeleteProduct(product._id)}>Delete</button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </dialog>
+                                            </tr>
+
+                                        ) :
+                                            <div className='my-16 text-center text-xl uppercase text-rose-500'> No products Found! </div>
+                                    }
 
 
-                        </tbody>
+                                </tbody>
 
 
-                    </table>
-                </div>
+                            </table>
+                        </div>
+                }
             </div>
         </div>
     )
